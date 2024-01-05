c33839b1b1d045d1fa880e88bca822086a1f3b7d
LeonardoDuarte <leopd_00@hotmail.com>
2024-01-05 17:37:36 -0300

    definitive commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    id: 0,
+    title:
+      'Não sabe o que é angular? aqui abaixo você encontra um breve resumo do que é!',
+    description:
+      'Angular é um framework de código aberto desenvolvido pelo Google para acriação de aplicativos dinâmicos e interativos da web. Ele se baseia emTypeScript, uma linguagem superset do JavaScript, e utiliza uma abordagemorientada a componentes para a construção de interfaces. O Angular ofereceuma estrutura sólida para desenvolvimento, incluindo suporte agerenciamento de estado, roteamento, validação de formulários e muitomais. Além disso, sua arquitetura modular e o poderoso sistema de injeçãode dependência facilitam a manutenção de aplicações complexas.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSknTsWM2nkoZ8PlwTkGRQqN2Zcjag4fj7RA&usqp=CAU',
+  },
+  {
+    id: 1,
+    title:
+      'Não sabe o que é angular? aqui abaixo você encontra um breve resumo do que é!',
+    description:
+      'Angular é um framework de código aberto desenvolvido pelo Google para acriação de aplicativos dinâmicos e interativos da web. Ele se baseia emTypeScript, uma linguagem superset do JavaScript, e utiliza uma abordagemorientada a componentes para a construção de interfaces. O Angular ofereceuma estrutura sólida para desenvolvimento, incluindo suporte agerenciamento de estado, roteamento, validação de formulários e muitomais. Além disso, sua arquitetura modular e o poderoso sistema de injeçãode dependência facilitam a manutenção de aplicações complexas.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSknTsWM2nkoZ8PlwTkGRQqN2Zcjag4fj7RA&usqp=CAU',
+  },
+  {
+    id: 2,
+    title:
+      'Não sabe o que é angular? aqui abaixo você encontra um breve resumo do que é!',
+    description:
+      'Angular é um framework de código aberto desenvolvido pelo Google para acriação de aplicativos dinâmicos e interativos da web. Ele se baseia emTypeScript, uma linguagem superset do JavaScript, e utiliza uma abordagemorientada a componentes para a construção de interfaces. O Angular ofereceuma estrutura sólida para desenvolvimento, incluindo suporte agerenciamento de estado, roteamento, validação de formulários e muitomais. Além disso, sua arquitetura modular e o poderoso sistema de injeçãode dependência facilitam a manutenção de aplicações complexas.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSknTsWM2nkoZ8PlwTkGRQqN2Zcjag4fj7RA&usqp=CAU',
+  },
+];
